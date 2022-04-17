c96ce182a7d3669a77b640ca4833dc607bf7fa1a
NicholasCH <nicholasch96@gmail.com>
2022-04-17 14:11:20 -0500

    route weight test:

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
 router.use((req, res) => {
   res.status(404).json({message: '404 Error!(index level)'});
 });
-router.use('/api', apiRoutes);
 
 
 
