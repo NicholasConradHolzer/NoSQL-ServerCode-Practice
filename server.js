b3912651ef79e61cdf3609ab0f2f7f62e8551e6f
NicholasCH <nicholasch96@gmail.com>
2022-04-17 16:14:13 -0500

    modularize mongo connect

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
+// const mongoose = require('mongoose');
+// const compression = require("compression");
+const mongo = require ('./config/mongo')
+const route = require('./routes')
 const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/thoughtDB'
+const app = express();
+// app.use(compression());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(route);
 // Use this to log mongo queries being executed!
+// const ion =
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/thoughtDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// mongoose.set('debug', true);
+// if (ion) {
+//   app.listen(PORT, () =>
+//     console.log(`Server listening at http://localhost:${PORT}`)
+//   );
+// } else {
+//   console.log('Connection to the database failed.');
+// }
+
+mongo.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
-
-mongoose.set('debug', true);
-
-
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
