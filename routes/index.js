585c849d7330e5fcbb2a767f4d8a7e237eac7864
NicholasCH <nicholasch96@gmail.com>
2022-04-17 16:25:20 -0500

    adjust approach

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 router.use((req, res) => {
-  res.status(404).json({message: '404 Error!(index level)'});
+  return res.send('wrong route')
+  // res.status(404).json({message: '404 Error!(index level)'});
 });
 
 
