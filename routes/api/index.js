a25a9a221aec98d5df3a6c8a951f0528d0a94262
NicholasCH <nicholasch96@gmail.com>
2022-04-17 13:47:51 -0500

    m

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./user-rt');
 const thoughtRoutes = require('./thought-rt');
+const userRoutes = require('./user-rt');
 
+router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 // console.log(userRoutes);
-router.use('/thoughts', thoughtRoutes);
 // console.log(thoughtRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
 router.use((req, res) => {
   res.status(404).json({message: '404 Error!(index level)'});
 });
+router.use('/api', apiRoutes);
+
+
 
 module.exports = router;
