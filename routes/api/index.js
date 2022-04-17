4527e8a9247a681c7d322dd65fc505a3cbb2a18e
NicholasCH <nicholasch96@gmail.com>
2022-04-17 09:07:51 -0500

    cleaning routing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./users.js');
-const thoughtRoutes = require('./thoughts.js');
+const userRoutes = require('./users');
+const thoughtRoutes = require('./thoughts');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
