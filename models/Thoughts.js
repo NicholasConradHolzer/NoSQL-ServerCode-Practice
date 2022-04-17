c22ad1676d390466d31ff53b112998d1388c3392
NicholasCH <nicholasch96@gmail.com>
2022-04-17 10:36:45 -0500

    fixed date format routing

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,5 @@
 const {Schema, model, Types} = require('mongoose');
-const dateFormat = require('./utils/dateFormat');
+const dateFormat = require('../utils/dateFormat');
 let ReactionSchema = require('./Reactions')
 const ThoughtSchema = new Schema({
     thoughtId:{
